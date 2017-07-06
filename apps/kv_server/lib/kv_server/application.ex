64339596481999bf95a7c87f84d58ea37c667a35
Marius Lueck <mluecksen@gmail.com>
2017-07-06 11:54:35 +0200

    some stuff

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,12 +6,14 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    import Supervisor.Spec
 
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
       # worker(KVServer.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
--- mix.exs
+++ mix.exs
@@ -21,6 +21,6 @@ defmodule KvUmbrella.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:ex_doc, "~> 0.14", only: :dev, runtime: false}]
   end
 end
